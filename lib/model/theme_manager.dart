0335eb6ebc3287ad110d6701dc77614cec3e50fa
lightScout <juanmullercs@hotmail.com>
2021-02-04 09:04:54 +0000

    .update theme manager logic

--- lib/model/theme_manager.dart
+++ lib/model/theme_manager.dart
@@ -1,11 +1,10 @@
-import 'package:ciao_app/model/store_manager.dart';
-import 'package:ciao_app/others/constants.dart';
 import 'package:flutter/material.dart';
+import 'store_manager.dart';
 
 class ThemeNotifier with ChangeNotifier {
-  final darkTheme = ThemeData.dark();
+  // final darkTheme = ThemeData.dark();
 
-  final lightTheme = ThemeData.light();
+  // final lightTheme = ThemeData.light();
 
   ThemeData _themeData;
   String _themeMode;
@@ -16,11 +15,11 @@ class ThemeNotifier with ChangeNotifier {
       print('value read from storage: ' + value.toString());
       var themeMode = value ?? 'light';
       if (themeMode == 'light') {
-        _themeData = lightTheme;
+        // _themeData = lightTheme;
         _themeMode = 'light';
       } else {
         print('setting dark theme');
-        _themeData = darkTheme;
+        // _themeData = darkTheme;
         _themeMode = 'dark';
       }
       notifyListeners();
@@ -32,14 +31,14 @@ class ThemeNotifier with ChangeNotifier {
   }
 
   void setDarkMode() async {
-    _themeData = darkTheme;
+    // _themeData = darkTheme;
     _themeMode = 'dark';
     StorageManager.saveData('themeMode', 'dark');
     notifyListeners();
   }
 
   void setLightMode() async {
-    _themeData = lightTheme;
+    // _themeData = lightTheme;
     _themeMode = 'light';
     StorageManager.saveData('themeMode', 'light');
     notifyListeners();
